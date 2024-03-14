c15a0d6081e64fd861b63984a1206093d6f2aa02
Marcel Koch <marcel.koch@kit.edu>
2024-03-14 09:20:06 +0100

    update benchmark script

--- benchmarking.py
+++ benchmarking.py
@@ -1,26 +1,29 @@
 #! /usr/bin/env python3
+import argparse
 import os
 import subprocess
 import uuid
 
 DIM = 3
 TARGET_SIZES = [1_000, 10_000, 100_000]
-REORDERINGS = ["none", "rcm", "amd"]
+REORDERINGS = ["none", "rcm", "amd", "metis"]
 SYM_TYPE = ["symmetric", "near_symmetric", "general"]
-
-PREFIX = "packages/shylu/shylu_dd/frosch/test/SolverFactory"
-EXE = "ShyLU_DDFROSch_solverfactory.exe"
+DRY_RUN = False
 
 
-def size(ts):
+def size_factory(ts):
     return round(ts ** (1. / DIM))
+
+
+def size_thyra(ts):
+    return round((ts / 3) ** (1. / DIM))
 
 
 def build_filename(**kwargs):
     return "_".join(f"{k}={v}" for k, v in sorted(kwargs.items()))
 
 
-def parameter_list(*, reordering="none", sym_type="general"):
+def parameter_list_factory(*, reordering="none", sym_type="general"):
     return f"""<ParameterList name="Solver Factory Example">
     <Parameter name="SolverType" type="string" value="GinkgoSolver"/>
     <ParameterList name="GinkgoSolver">
@@ -31,36 +34,148 @@ def parameter_list(*, reordering="none", sym_type="general"):
     """
 
 
-for ts in TARGET_SIZES:
-    for reordering in REORDERINGS:
-        if reordering == "none" and ts == 1_000_000:
-            continue
+def parameter_list_thyra(*, reordering="none", sym_type="general"):
+    return f"""<ParameterList name="Thyra Example">
+    <Parameter name="Linear Solver Type" type="string" value="Belos"/>
+    <ParameterList name="Linear Solver Types">
+        <ParameterList name="Belos">
+            <Parameter name="Solver Type" type="string" value="Block GMRES"/>
+            <ParameterList name="Solver Types">
+                <ParameterList name="Block GMRES">
+                    <Parameter name="PreconditionerPosition" type="string" value="left"/>
+                    <Parameter name="Block Size" type="int" value="1"/>
+                    <Parameter name="Convergence Tolerance" type="double" value="1e-4"/>
+                    <Parameter name="Maximum Iterations" type="int" value="100"/>
+                    <Parameter name="Output Frequency" type="int" value="1"/>
+                    <Parameter name="Show Maximum Residual Norm Only" type="bool" value="1"/>
+                </ParameterList>
+            </ParameterList>
+        </ParameterList>
+    </ParameterList>
+    <Parameter name="Preconditioner Type" type="string" value="FROSch"/>
+    <ParameterList name="Preconditioner Types">
+        <ParameterList name="FROSch">
+            <Parameter name="FROSch Preconditioner Type" type="string" value="GDSWPreconditioner"/>
 
-        for sym_type in SYM_TYPE:
-            if sym_type == "general" and ts == 1_000_000:
+            <ParameterList name="AlgebraicOverlappingOperator">
+                <ParameterList name="Solver">
+                    <Parameter name="SolverType" type="string" value="GinkgoSolver"/>
+                    <ParameterList name="GinkgoSolver">
+                        <Parameter name="Reordering" type="string" value="{reordering}"/>
+                        <Parameter name="SymbolicType" type="string" value="{sym_type}"/>
+                    </ParameterList>
+                </ParameterList>
+            </ParameterList>
+
+            <ParameterList name="GDSWCoarseOperator">
+                <ParameterList name="Blocks">
+                    <ParameterList name="1">
+                        <Parameter name="Use For Coarse Space" type="bool" value="true"/>
+                        <Parameter name="Rotations" type="bool" value="true"/>
+                    </ParameterList>
+                </ParameterList>
+
+                <ParameterList name="ExtensionSolver">
+                    <Parameter name="SolverType" type="string" value="GinkgoSolver"/>
+                    <ParameterList name="GinkgoSolver">
+                        <Parameter name="Reordering" type="string" value="{reordering}"/>
+                        <Parameter name="SymbolicType" type="string" value="{sym_type}"/>
+                    </ParameterList>
+                </ParameterList>
+
+                <ParameterList name="Distribution">
+                    <Parameter name="Type" type="string" value="linear"/>
+                    <Parameter name="NumProcs" type="int" value="1"/>
+                    <Parameter name="Factor" type="double" value="1.0"/>
+                    <Parameter name="GatheringSteps" type="int" value="1"/>
+                    <ParameterList name="Gathering Communication">
+                        <Parameter name="Send type" type="string" value="Send"/>
+                    </ParameterList>
+                </ParameterList>
+
+                <ParameterList name="CoarseSolver">
+                    <Parameter name="SolverType" type="string" value="GinkgoSolver"/>
+                    <ParameterList name="GinkgoSolver">
+                        <Parameter name="Reordering" type="string" value="{reordering}"/>
+                        <Parameter name="SymbolicType" type="string" value="{sym_type}"/>
+                    </ParameterList>
+                </ParameterList>
+            </ParameterList>
+        </ParameterList>
+    </ParameterList>
+</ParameterList>
+"""
+
+
+def run(*, size, parameter_list, app, prefix, exe, np):
+    for ts in TARGET_SIZES:
+        for reordering in REORDERINGS:
+            if reordering == "none" and ts == 1_000_000:
                 continue
 
-            actual_size = size(ts)
+            for sym_type in SYM_TYPE:
+                if sym_type == "general" and ts == 1_000_000:
+                    continue
 
-            tmp_file_name = uuid.uuid4().hex[:6]
-            with open(tmp_file_name, "w") as tmp_file:
-                tmp_file.write(parameter_list(reordering=reordering, sym_type=sym_type))
+                actual_size = size(ts)
 
-            args = [f"{PREFIX}/{EXE}", f"--M={actual_size}", f"--PLIST={tmp_file_name}", "--USETPETRA", f"--DIM={DIM}"]
+                tmp_file_name = uuid.uuid4().hex[:6]
+                with open(tmp_file_name, "w") as tmp_file:
+                    tmp_file.write(parameter_list(reordering=reordering, sym_type=sym_type))
 
-            try:
+                args = ["mpirun", "-n", f"{np}", f"{prefix}/{exe}", f"--M={actual_size}", f"--PLIST={tmp_file_name}",
+                        "--USETPETRA", f"--DIM={DIM}"]
+
                 print("Running: ", args)
                 with open(tmp_file_name, "r") as file:
                     print(file.read())
 
-                for _ in range(TARGET_SIZES[-1] // ts):
-                    cp = subprocess.run(args, capture_output=True, universal_newlines=True, check=True)
+                if not DRY_RUN:
+                    try:
+                        cp = subprocess.run(args, capture_output=True, universal_newlines=True, check=True)
 
-                with open(build_filename(size=actual_size, reordering=reordering, sym_type=sym_type), "w") as file:
-                    file.write(cp.stdout)
+                        with open(build_filename(app=app, size=actual_size, reordering=reordering, sym_type=sym_type),
+                                  "w") as file:
+                            file.write(cp.stdout)
 
-                print(" " * 4, "Success")
-            except subprocess.CalledProcessError as e:
-                print(e.stderr)
-            finally:
+                        print(" " * 4, "Success")
+                    except subprocess.CalledProcessError as e:
+                        print(e.stderr)
+
                 os.remove(tmp_file_name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", type=int, default=0)
+    parser.add_argument("--dry-run", action="store_true")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--factory", action="store_true")
+    group.add_argument("--thyra", action="store_true")
+
+    args = parser.parse_args()
+
+    global DRY_RUN
+    DRY_RUN = args.dry_run
+
+    if args.factory:
+        prefix = "packages/shylu/shylu_dd/frosch/test/SolverFactory"
+        exe = "ShyLU_DDFROSch_solverfactory.exe"
+        if args.n == 0:
+            args.n = 1
+        run(size=size_factory, parameter_list=parameter_list_factory, app="factory", prefix=prefix, exe=exe, np=args.n)
+
+    if args.thyra:
+        prefix = "packages/shylu/shylu_dd/frosch/test/Thyra_Xpetra_Elasticity"
+        exe = "ShyLU_DDFROSch_thyra_xpetra_elasticity.exe"
+        if args.n == 0:
+            args.n = 8
+
+        global REORDERINGS, SYM_TYPE
+        REORDERINGS = ["metis"]
+        SYM_TYPE = ["near_symmetric"]
+
+        run(size=size_thyra, parameter_list=parameter_list_thyra, app="thyra", prefix=prefix, exe=exe, np=args.n)
+
+if __name__ == "__main__":
+    main()
